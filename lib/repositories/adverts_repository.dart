d31cb0f636b1e3dfbca6227f3abe8936da61706c
Raimundo-Martins <cosmomartinsj@gmail.com>
2021-02-26 14:14:31 -0300

    Novas implementações.

--- lib/repositories/adverts_repository.dart
+++ lib/repositories/adverts_repository.dart
@@ -1,7 +1,39 @@
+import 'dart:io';
+
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 import 'package:xlo/models/adverts.dart';
+import 'package:path/path.dart' as path;
+import 'package:xlo/repositories/parse_errors.dart';
 
 class AdvertsRepository {
-  void save(Adverts adverts){
-    
+  void save(Adverts adverts) async {
+    final parseImages = await saveImages(adverts.images);
+  }
+
+  Future<List<ParseFile>> saveImages(List images) async {
+    final parseImages = <ParseFile>[];
+
+    try {
+      for (final image in images) {
+        if (image is File) {
+          final parseFile = ParseFile(image, name: path.basename(image.path));
+          final response = await parseFile.save();
+          if (!response.success)
+            return Future.error(
+                ParseErrors.getDescription(response.error.code));
+
+          parseImages.add(parseFile);
+        } else {
+          final parseFile = ParseFile(null);
+          parseFile.name = path.basename(image);
+          parseFile.url = image;
+          parseImages.add(parseFile);
+        }
+      }
+    } catch (e) {
+      return Future.error('Falha ao salvar imagens!');
+    }
+
+    return parseImages;
   }
 }
